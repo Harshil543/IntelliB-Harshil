fc33b64b15488c38020ebcc619ba27a12f5decb2
arti-techavtar <artitechavtar@gmail.com>
2024-09-05 20:30:53 +0530

    [add]: Apply Filter on Payment Analysis

--- src/components/CommonComponents/PaymentAnalysis.tsx
+++ src/components/CommonComponents/PaymentAnalysis.tsx
@@ -13,10 +13,10 @@ import {
   pieChartDataYearly,
   pieChartDataMonthly,
   pieChartDataQuarterly
-} from '@/constants/data.constants'; // Import different datasets
+} from '@/constants/data.constants';
 import Image from 'next/image';
 import verticleSeprator from '@assets/images/verticleSeprator.png';
-import Select from 'react-select'; // Import react-select for dropdown
+import Select from 'react-select';
 
 ChartJS.register(Title, Tooltip, Legend, ArcElement);
 
@@ -61,51 +61,49 @@ const PaymentAnalysis = () => {
   }
 
   return (
-    <>
-      <Card className="col-span-4 md:col-span-3">
-        <CardHeader className="flex flex-row items-center justify-between">
-          <CardTitle className="inline-block">Payment Analysis</CardTitle>
-          {/* Dropdown for filtering */}
-          <Select
-            options={filterOptions}
-            defaultValue={filterOptions[0]}
-            onChange={handleFilterChange}
-            className="inline-block h-10 w-40 text-sm"
-            isSearchable={false}
-          />
-        </CardHeader>
-        <CardContent className="flex h-[50%] items-center justify-center">
-          <PieChart data={chartData} />
-        </CardContent>
-        <CardFooter className="flex items-center justify-center align-middle">
-          <Card className="flex w-fit justify-center text-center align-middle">
-            <div>
-              <CardHeader className="flex flex-row items-center justify-center gap-0 space-y-0 pb-0">
-                <CardTitle className="text-sm font-medium text-muted-foreground">
-                  Payment Done
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="text-lg font-bold">63%</div>
-              </CardContent>
-            </div>
-            <div className="my-auto">
-              <Image src={verticleSeprator} alt="seprator" className="h-16" />
-            </div>
-            <div>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-0">
-                <CardTitle className="text-sm font-medium text-muted-foreground">
-                  Payment Pending
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <div className="text-lg font-bold">25%</div>
-              </CardContent>
-            </div>
-          </Card>
-        </CardFooter>
-      </Card>
-    </>
+    <Card className="col-span-4 md:col-span-3">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <CardTitle className="inline-block">Payment Analysis</CardTitle>
+        {/* Dropdown for filtering */}
+        <Select
+          options={filterOptions}
+          defaultValue={filterOptions[0]}
+          onChange={handleFilterChange}
+          className="inline-block h-10 w-40 text-sm"
+          isSearchable={false}
+        />
+      </CardHeader>
+      <CardContent className="flex h-[50%] items-center justify-center">
+        <PieChart data={chartData} />
+      </CardContent>
+      <CardFooter className="flex items-center justify-center align-middle">
+        <Card className="flex w-fit justify-center text-center align-middle">
+          <div>
+            <CardHeader className="flex flex-row items-center justify-center gap-0 space-y-0 pb-0">
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Payment Done
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-lg font-bold">63%</div>
+            </CardContent>
+          </div>
+          <div className="my-auto">
+            <Image src={verticleSeprator} alt="seprator" className="h-16" />
+          </div>
+          <div>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-0">
+              <CardTitle className="text-sm font-medium text-muted-foreground">
+                Payment Pending
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="text-lg font-bold">25%</div>
+            </CardContent>
+          </div>
+        </Card>
+      </CardFooter>
+    </Card>
   );
 };
 
