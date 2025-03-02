5f39b610d0a3427b7db3d9f447674420862a0778
Harshil <sondagarharshil0@gmail.com>
2025-03-03 02:06:48 +0530

    [update]: sidebar updated.

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -27,7 +27,7 @@ export default function Sidebar({ className, data }: SidebarProps) {
       case 'super_admin':
         return navItems?.super_admin;
       case 'system_admin':
-        return navItems?.property_admin;
+        return navItems?.system_admin;
       // /system_admin;
       case 'tenant-admin':
         return navItems?.tenant_admin;
